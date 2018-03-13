a39475e602db126a1e5622364a051ac1a6c24975
shamanoff <shamanoff@gmail.com>
2018-03-13 20:15:49 +0200

    home

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('serve', ['sass'], function() {
         server: "./src"
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']).on('change', browserSync.reload);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
